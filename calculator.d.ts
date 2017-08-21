c6980f32f0b34fba6d3fc40e833e1e1e86c44ef0
Suhas Chatekar <suhas.chatekar@Suhas-Chatekar.local>
2017-08-21 16:12:59 +0100

    Added type definitions

---
+++ calculator.d.ts
@@ -0,0 +1,8 @@
+declare  module calculator {
+  export function add(a :number, b :number): number
+  export function subtract(a :number, b :number): number
+  export function multiply(a :number, b :number): number
+  export function divide(a :number, b :number): number
+}
+
+export = calculator;
